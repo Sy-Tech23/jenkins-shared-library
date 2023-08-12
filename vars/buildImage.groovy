2dfac201af655c77a64a5370a8a234fa714aa0be
Sy-Tech23 <sy_tech23@outlook.com>
2023-08-12 14:51:59 +0300

    string error resolved

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #! usr/bin/env groovy
 
-def call(string imageName) {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t $imageName ."
